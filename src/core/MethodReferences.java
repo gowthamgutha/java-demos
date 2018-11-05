c6048aa45674a7403d82aa181c5eb063c2f8eb36
gowthamgutha <44779402+gowthamgutha@users.noreply.github.com>
2018-11-05 21:14:56 +0530

    Create MethodReferences.java

---
+++ src/core/MethodReferences.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class MethodReferencesDemo
+{
+	public static void main(String args[])
+	{
+	List<String> namesList=new ArrayList<String>();
+	
+	namesList.add("Rama");
+	namesList.add("Sita");
+	namesList.add("Lakshmana");
+	namesList.add("Bharatha");
+	namesList.add("Sathrugna");
+
+	// call the println() method of System.out object
+	namesList.forEach(System.out::println);
+
+  // call the static printString method of MethodReferencesDemo class
+	namesList.forEach(MethodReferencesDemo::printString);
+	}
+
+	public static void printString(String st)
+	{
+	System.out.println(st);
+	}
+}
